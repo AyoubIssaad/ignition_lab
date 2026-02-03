b7c94bc9bd9a2caef1d0fb65d2d0da9ff661e943
ayoubissaad <ayoub.issaad@gmail.com>
2026-02-03 11:18:11 +0100

    test

---
+++ cache.py
@@ -0,0 +1,16 @@
+# no_cache_server.py
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+
+
+class NoCacheHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header(
+            "Cache-Control",
+            "no-store, no-cache, must-revalidate, max-age=0",
+        )
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+
+ThreadingHTTPServer(("0.0.0.0", 8000), NoCacheHandler).serve_forever()
